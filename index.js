b61e9ddd8d0959abace75a22bd61de7a51c5b9c4
Rahman-Altaf <rahmanch.311@gmail.com>
2024-08-21 01:23:48 +0500

    New Data Post in the form of (Data FORM ) through Post

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const PORT = 8000;
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
 app.get("/users", (req, res) => {
   const html = `
     <ul>
@@ -10,16 +15,28 @@ app.get("/users", (req, res) => {
 `;
   res.send(html);
 });
+// Routes
 app.get("/api/users", (req, res) => {
   return res.json(users);
 });
+// Routes
 
 app.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
-  const user = users.find((user)=> user.id===id)
+  const user = users.find((user) => user.id === id);
   return res.json(user);
 });
+// Routes
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    // console.log("body", body);
+    return res.json({ status: "Status Pending" });
+  });
+});
 
+// Listner
 app.listen(PORT, () => {
   console.log(`Server Started Successfully : ${PORT}`);
 });
